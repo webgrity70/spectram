29fedcb01e14c999e082f69dde4460224105f8a7
web-ram <webgrity70@gmail.com>
2022-02-04 17:34:57 +0530

    front page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './common/Header';
 import './app.css'
+import Frontpage from './components/Frontpage';
 function App() {
-	return <Header />
+	return (
+		<>
+		<Header />
+		<Frontpage />
+		</>
+	)
 }
 
 export default App;
